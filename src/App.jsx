b9d3959d1c5e44358629a9768c130a43a00fbf6f
Christopher Abraham <christopherabraham8@gmail.com>
2022-03-28 22:36:13 +0100

    added preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Components/header/header';
 import Nav from './Components/nav/Nav';
 import About from './Components/about/about';
@@ -8,10 +8,37 @@ import Portfolio from './Components/Portfolio/Portfolio'
 import Testimonial from './Components/testimonial/testimonial'
 import Contact from './Components/contact/contact'
 import Footer from './Components/footer/footer'
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    },10000)
+  
+  }, [])
+  
+
+
   return (
     <div>     
+      {
+        loading ?
+
+        ( <ClimbingBoxLoader
+          size={30}
+          color={'#123abc'}
+          loading={loading}
+
+          />
+
+        ):
+        
+          (
+      
       <>
       <Header />
         <Nav />
@@ -23,8 +50,8 @@ const App = () => {
         <Testimonial />
         <Footer />
       </> 
-        
-   
+          )
+      }
       
     </div>
   )
